609efe68fe6c0cf7699897282de8772aaf44be2a
Stefan Stoychev <stefan.stoychev44@gmail.com>
2017-02-19 16:07:45 +0200

    Fixed dog animation.

---
+++ Assets/Scripts/SplashScreenController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenController : MonoBehaviour
+{
+    public string MainMenuSceneName = "Menu";
+
+    public void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space))
+            return;
+
+        SceneManager.LoadScene(MainMenuSceneName);
+    }
+}
